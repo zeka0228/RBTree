5710b6b56b5c5585ca226db60418e55414bbf9de
zeka0228 <jhim0228@gmail.com>
2025-04-18 00:25:08 +0900

    insert_comp_maybe?

--- rbtree.c
+++ rbtree.c
@@ -4,6 +4,8 @@
 
 rbtree *new_rbtree(void) {
   rbtree *p = (rbtree *)calloc(1, sizeof(rbtree));
+  p->nil = malloc(sizeof(node_t)); //센티널만들기
+  p->nil->color = RBTREE_BLACK;  //블랙 고정정
   int item;
   printf ("트리를 만드려 하시는군요! 트리에 넣을 값을 알려주시면 도와드릴게요! : ");
   while (scanf("%d", item) > 0){
@@ -14,13 +16,13 @@ rbtree *new_rbtree(void) {
 }
 
 
-node_t*makenode(int item){
+node_t*makenode(rbtree *t,int item){
   node_t *node = malloc(sizeof(node_t));
   node->key = item;
   node->color = RBTREE_RED;
-  node->left = NULL;
-  node->right = NULL;
-  node->parent = NULL;
+  node->left = t->nil;
+  node->right = t->nil;
+  node->parent = t->nil;
   return node;
 }
 
@@ -32,13 +34,17 @@ void delete_rbtree(rbtree *t) {
 
 node_t *rbtree_insert(rbtree *t, const key_t key) {
   node_t *checker = t->root;
-  node_t *new_node = makenode(key);
+  
   //루트가 존재하지 않을 시 예외처리
   if (checker == NULL){        
-    checker = makenode(key);
-    return checker;
+    checker = makenode(t,key);
+    checker->color = RBTREE_BLACK;
+    t->root = checker;
+    
+    return t->root;
   }
 
+  node_t *new_node = makenode(t, key);
   //제자리 찾아갈때까지 반복복
   while (1)
   {
@@ -67,21 +73,83 @@ node_t *rbtree_insert(rbtree *t, const key_t key) {
 
     // 임시입니다. 임시입니다. 중복값 나오면 일단 레프트에 넣자 
     else{
-      new_node->left = checker->left; 
-      checker->left = new_node;
+      new_node->right = checker->right; 
+      checker->right = new_node;
 
       break;
     }
   }
   new_node->parent = checker;
-  INSERTChecking(&(new_node));
+
+  //부모가 레드일때만 체크, 블랙은 패스, 루트인건 위에서 예외처리 했으니 경우의 수 없음
+  if (new_node->parent->color == RBTREE_RED) 
+    INSERTChecking(new_node);
   return t->root;
 }
 
 
-node_t INSERTChecking(node_t **node){
+//삽입 조건 만족 확인
+void INSERTChecking(node_t *node){
+  //바텀 업 구상이기에 루트까지 올라가면 블랙처리 후 리턴
+  if (node->parent->key == NULL){ //수정 필요 루트의 대가리는 NIL
+    node->color = RBTREE_BLACK;
+    return;
+  }
 
+  //RnR 위반 시
+  if (node->color = RBTREE_RED && (node->left->color==RBTREE_RED || node->right->color == RBTREE_RED)){
+    //노드가 우측일 때
+    if(node->key >= node->parent->key){
 
+      //형제 노드가 레드일 때
+      if(node->parent->left->color == RBTREE_RED){
+        node->color= RBTREE_BLACK;
+        node->parent->left->color = RBTREE_BLACK;
+        node->parent->color = RBTREE_RED;
+      }
+
+      else{ //자식도 동일 방향일 때
+        node->parent->color = RBTREE_RED;
+        if(node->right->color == RBTREE_RED){
+          node->color = RBTREE_BLACK;
+          L_rocate(node->parent); //좌회전
+        }
+        else{
+          node->left->color = RBTREE_BLACK;
+          R_rocate(node->left); //혹은 우회전
+          L_rocate(node->parent->parent); //다시 좌회전..인데 자식노드랑 바꼈으니 부모의 부모로 회전
+        }
+
+      }
+
+    }
+
+    //노드가 좌측일 때때
+    else{
+      //형제 노드가 레드일 때
+      if(node->parent->right->color == RBTREE_RED){
+        node->color= RBTREE_BLACK;
+        node->parent->right->color = RBTREE_BLACK;
+        node->parent->color = RBTREE_RED;
+      }
+
+      else{ //자식도 동일 방향일 때
+        node->parent->color = RBTREE_RED;
+        if(node->left->color == RBTREE_RED){
+          node->color = RBTREE_BLACK;
+          R_rocate(node->parent); //우우회전
+        }
+        else{
+          node->right->color = RBTREE_BLACK;
+          L_rocate(node->right); //좌회전
+          R_rocate(node->parent->parent); //혹은 우회회전..인데 자식노드랑 바꼈으니 부모의 부모로 회전
+        } 
+      }
+    }
+
+  }
+  
+  INSERTChecking(node->parent);
 }
 
 
